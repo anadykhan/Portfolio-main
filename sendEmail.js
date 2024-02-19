70b1776dbd6201848f8cbba701fdf68a5652e61f
Aiyoun Khan <aiyounkhan@Aiyouns-Mac-mini.local>
2024-02-19 17:29:56 +0600

    Again commiting for not lefting over any changes

--- sendEmail.js
+++ sendEmail.js
@@ -4,8 +4,6 @@ function takeInput() {
 
 // Attach an event listener to the submit button
 document.getElementById("submitBtn").addEventListener("click", function (event) {
-    // Prevent the default action of the link (in this case, navigating to #)
     event.preventDefault();
-    // Call the function to handle form submission
     takeInput()
 })
